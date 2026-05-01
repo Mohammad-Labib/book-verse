92a0ca5607702fc6d6e368b88c05bd98a44ba416
Mohammad-Labib <friendlabib@gmail.com>
2026-05-02 00:37:43 +0600

    login problem hocce

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      }, 
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/**",
+      },
       {
-        protocol: 'https',
-        hostname: 'images-na.ssl-images-amazon.com',
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+        pathname: "/**",
       },
     ],
   },
